a9c4d4f616ae269b0a22832d7fe88c7f7eec4a92
mayyad443 <72155491+mayyad443@users.noreply.github.com>
2022-04-02 14:31:05 -0400

    Update Program.cs

--- BooksExamples/DisplayTable/Program.cs
+++ BooksExamples/DisplayTable/Program.cs
@@ -6,17 +6,47 @@ using System.Windows.Forms;
 
 namespace DisplayTable
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DisplayAuthorsTable());
-        }
-    }
-}
+public partial class DisplayAuthorsTable : Form
+{
+
+public DisplayAuthorsTable()
+{
+DisplayAuthorsTable();
+} 
+
+
+private BooksDataContext database = new BooksDataContext();
+
+
+private void DisplayAuthorsTable_Load( object sender, EventArgs e )
+{
+
+authorBindingSource.DataSource =
+from author in database.Authors
+orderby author.AuthorID
+select author;
+} 
+
+
+private void authorBindingNavigatorSaveItem_Click(
+object sender, EventArgs e )
+{
+Validate(); 
+authorBindingSource.EndEdit(); 
+database.SubmitChanges(); 
+} 
+
+private void findButton_Click( object sender, EventArgs e )
+{
+
+authorBindingSource.DataSource =
+from author in database.Authors
+where author.LastName.StartsWith( findTextBox.Text )
+orderby author.AuthorID
+select author;
+} 
+} 
+
+} 
+
+
